a0942ef5b43b6421dc3da0e46454078e97e83e8c
Joao Lorenco <jllorenc00@gmail.com>
2024-09-03 21:21:44 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLinkActive,
+    RouterModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'consulta-cep';
+}
